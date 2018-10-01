8f0f7b1eef6724cd6489d4b72aafe1f6dafe6bdd
fabischapeli <fabischapeli97@gmail.com>
2018-10-01 14:59:18 +0300

    [Feature #160884872] delete comment

--- src/comments.py
+++ src/comments.py
@@ -1,13 +1,27 @@
 """define the comments model"""
-
+from users import get_user
 
 class Comments():
     """define and configure the comments model"""
 
-    comments = []  # comments list
     def __init__(self, author, created_at, comment_id, content):
         self.author = author
         self.created_at = created_at
         self.comment_id = comment_id
         self.content = content
-        self.comment = {}  # comment item
+        self.comments = []
+
+    def delete_comment(self, author, comment_id):
+        role = get_user(author)['role']
+        if role == 'admin' or role == 'moderator':
+            for comment in self.comments:
+                id = comment.get('id')
+                if id == comment_id:
+                    self.comments.remove(comment)
+        elif author == self.author:
+            for comment in self.comments:
+                id = comment.get('id')
+                if id == comment_id:
+                    self.comments.remove(comment)
+        else:
+            return 'you are not authorized to perform this task'
