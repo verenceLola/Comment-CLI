b26003d8d3a190586a29ae6764237dfe63b045b2
Darius-Ndubi <ndubidarius@gmail.com>
2018-10-01 15:37:51 +0300

    [Chore #160887936] Add application guide

--- app.py
+++ app.py
@@ -1,2 +1,20 @@
-from users import User 
+"""App selection inputs"""
 
+choice1 = input("To use this app please follow th11e guide bellow: \n Press: \n 1.To signup \n 2.To login \n")
+
+from src.users import User
+
+userAO = User()
+
+# def comment_choices():
+#     comment_choice=input("To create comment: \n Press:\n 1.To add comment \n 2.To edit comment")
+
+if int(choice) == 1:
+    userAO.save()
+    #comment_choices()
+
+elif int(choice) == 2:
+    userAO.login()
+
+
+    
